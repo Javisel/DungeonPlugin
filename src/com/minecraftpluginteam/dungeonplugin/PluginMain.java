8443100853cd95cc4ac955f4da414f9f85433082
Javisel <36491374+Javisel@users.noreply.github.com>
2020-10-20 14:21:04 -0400

    Added event handler for dungeon gen

--- src/com/minecraftpluginteam/dungeonplugin/PluginMain.java
+++ src/com/minecraftpluginteam/dungeonplugin/PluginMain.java
@@ -1,10 +1,26 @@
 package com.minecraftpluginteam.dungeonplugin;
 
+import com.minecraftpluginteam.dungeonplugin.common.EventHandler;
+import com.onarandombox.MultiverseCore.MultiverseCore;
+import org.bukkit.World;
+import org.bukkit.WorldType;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PluginMain extends JavaPlugin {
 
+    @Override
+    public void onDisable() {
+        getServer().getConsoleSender().sendMessage("Hi, the mod isn't here :(");
+    }
+
+    @Override
+    public void onEnable() {
+       getServer().getConsoleSender().sendMessage("Hi, the mod is here!");
+        getServer().getPluginManager().registerEvents(new EventHandler(), this);
+       MultiverseCore multiverseCore = (MultiverseCore) getServer().getPluginManager().getPlugin("Multiverse-Core");
 
 
 
+
+    }
 }
